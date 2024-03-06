1ddd84641c8dcb787677d7923ffc544adb8e20c2
YhongYhong <prommmm44@gmail.com>
2024-03-06 16:23:56 +0700

    Update comment property

--- WebApplicationProject/Models/Comment.cs
+++ WebApplicationProject/Models/Comment.cs
@@ -1,6 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using WebApplicationProject.Areas.Identity.Data;
+using WebApplicationProject.Data;
 
 namespace WebApplicationProject.Models
 {
@@ -10,10 +12,16 @@ namespace WebApplicationProject.Models
         public Guid Id { get; set; }
 
         [ForeignKey("User")]
-        public string UserID { get; set; }
+        public string UserID { get; set; }  // Foreign key referencing the user who authored the comment
         public ApplicationUser User { get; set; }
 
+        [ForeignKey("Event")]
+        public Guid? EventId { get; set; }
+        public Event Event { get; set; }
+
         [Required]
-        public string Detail { get; set; }
+        public string Detail { get; set; }  // Content of the comment
+
+        public DateTime CreatedAt { get; set; }  // Timestamp of when the comment was created
     }
 }
