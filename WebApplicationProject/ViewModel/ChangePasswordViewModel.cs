96334b2de6913921e625e16e073b2f22cab96f9a
pkstpm <pongsapat028@gmail.com>
2024-03-12 20:30:22 +0700

    fix script

---
+++ WebApplicationProject/ViewModel/ChangePasswordViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplicationProject.ViewModel
+{
+    public class ChangePasswordViewModel
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
